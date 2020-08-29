15e4c1bd760743a9e06d729458268ec3768666c8
GEORGIOS PANAGIOTINOS <gpanagiotinos@GEORGIOSs-MacBook-Pro.local>
2020-08-29 12:34:39 +0300

    Build

---
+++ webpack.config.prod.js
@@ -0,0 +1,77 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+    mode: 'none',
+    entry: {
+        app: path.join(__dirname, 'src', 'index.tsx'),
+    },
+    target: 'web',
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js'],
+        alias: {
+            '@': path.resolve(__dirname, 'src/')
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts(x?)$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'ts-loader',
+                    },
+                ],
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader',
+                ],
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                  { loader: "style-loader" },
+                  { loader: "css-loader" },
+                  { loader: "sass-loader" }
+                ]
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            favicon: './src/favicon.ico'
+
+        }),
+        new webpack.HashedModuleIdsPlugin()
+    ],
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    optimization: {
+        runtimeChunk: 'single',
+        splitChunks: {
+          chunks: 'all',
+          maxInitialRequests: Infinity,
+          minSize: 0,
+          cacheGroups: {
+            vendor: {
+              test: /[\\/]node_modules[\\/]/,
+              name(module) {
+                // get the name. E.g. node_modules/packageName/not/this/part.js
+                // or node_modules/packageName
+                const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+    
+                // npm package names are URL-safe, but some servers don't like @ symbols
+                return `npm.${packageName.replace('@', '')}`;
+              },
+            },
+          },
+        },
+      },
+}
